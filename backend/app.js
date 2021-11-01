79bbd97a9e4860ceb8418dca474bc76316572185
ayush-rathore <aayushh79@gmail.com>
2021-11-02 01:05:32 +0530

    Add app.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const cors = require("cors");
+
+const router = require("./routes/route");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+module.exports = app;
