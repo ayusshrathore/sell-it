a79f2efb29383ba0c1c5fb6b9d5d1c81274c1f50
ayush-rathore <aayushh79@gmail.com>
2021-11-02 01:01:28 +0530

    Use listingAPI

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -5,11 +5,39 @@ import { FlatList, StyleSheet } from "react-native";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
-import listings from "../components/listingData.js";
+import { useState, useEffect } from "react";
+import listingAPI from "../api/listingAPI";
+import AppText from "../components/AppText";
+import Button from "../components/AppButton";
+import ActivityIndicator from "../components/ActivityIndicator";
+import useApi from "../hooks/useApi";
+// import listings from "../components/listingData.js";
 
 function ListingScreen({ navigation }) {
+	const {
+		data: listings,
+		error,
+		loading,
+		request: loadListing,
+	} = useApi(listingAPI.getListings);
+
+	useEffect(() => {
+		loadListing();
+	}, []);
+
 	return (
 		<Screen style={styles.screen}>
+			{error && (
+				<>
+					<AppText>Couldn't fetch the items.</AppText>
+					<Button
+						title="Retry"
+						onPress={loadListing}
+						color="primary"
+					/>
+				</>
+			)}
+			<ActivityIndicator visible={loading} />
 			<FlatList
 				data={listings}
 				keyExtractor={(listing) => listing.id.toString()}
