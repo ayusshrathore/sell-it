137bb4f346cccf2022037a16cdff5da1c51cd8cc
ayush-rathore <aayushh79@gmail.com>
2021-11-02 00:59:39 +0530

    Add listing api

---
+++ app/api/listingAPI.js
@@ -0,0 +1,23 @@
+import client from "./client";
+
+const getListings = () => client.get("/listing");
+
+const addListings = (listing) => {
+	const data = new FormData();
+	data.append("title", listing.title);
+	data.append("price", listing.price);
+	// data.append("categoryId", listing.category.value);
+	// data.append("description", listing.description);
+
+	// listing.images.forEach((image, index) => {
+	// 	data.append("images", {
+	// 		name: "image" + index,
+	// 		type: "image/jpeg",
+	// 		uri: image,
+	// 	});
+	// });
+
+	return client.post("/listing/add", data);
+};
+
+export default { addListings, getListings };
