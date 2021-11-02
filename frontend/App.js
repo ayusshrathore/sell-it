bed00a3c85831d2665ffe3e75d03a637ba02f7a3
ayush-rathore <aayushh79@gmail.com>
2021-11-02 23:08:14 +0530

    Add status screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import OfflineStatus from "./app/components/OfflineStatus";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
 function App() {
 	return (
-		<NavigationContainer theme={navigationTheme}>
-			{/* <AuthNavigator /> */}
-			<AppNavigator />
-		</NavigationContainer>
+		<>
+			<OfflineStatus />
+			<NavigationContainer theme={navigationTheme}>
+				<AuthNavigator />
+				{/* <AppNavigator /> */}
+			</NavigationContainer>
+		</>
 	);
 }
 
