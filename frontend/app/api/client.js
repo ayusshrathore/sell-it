50db8cc2dc8b268f3234b4b7547ab0ed1187a915
ayush-rathore <aayushh79@gmail.com>
2021-11-02 23:09:23 +0530

    Add cache method

--- frontend/app/api/client.js
+++ frontend/app/api/client.js
@@ -1,7 +1,21 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 const apiClient = create({
 	baseURL: "http://192.168.1.7:8080/api/",
 });
+
+const get = apiClient.get;
+apiClient.get = async (url, params, axiosConig) => {
+	const response = await get(url, params, axiosConig);
+
+	if (response.ok) {
+		cache.store(url, response.data);
+		return response;
+	}
+
+	const data = await cache.get(url);
+	return data ? { ok: true, data } : response;
+};
 
 export default apiClient;
