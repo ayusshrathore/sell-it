c8eafc515a6634dafeb372c607ed6d2a88a3af99
ayush-rathore <aayushh79@gmail.com>
2021-11-02 23:07:17 +0530

    Add user controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,32 @@
+const User = require("../model/user");
+
+exports.registerUser = (req, res) => {
+	let { name, email, password } = req.body;
+	let user = new User({ name, email, password });
+	user.save()
+		.then((user) => {
+			if (user) {
+				console.log("User saved");
+				return res.status(200).send(user);
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			return res.status(500).send(err);
+		});
+};
+
+exports.loginUser = (req, res) => {
+	let { email, password } = req.body;
+	User.findOne({ email, password })
+		.then((user) => {
+			if (user) {
+				console.log("User logged in");
+				return res.status(200).send(user);
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			return res.status(500).send(err);
+		});
+};
