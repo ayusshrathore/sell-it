7d8b51c954ca4cce85b437efa24c652bb273f08e
ayush-rathore <aayushh79@gmail.com>
2021-11-02 23:07:58 +0530

    Add user routes

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express();
 const itemController = require("../controller/itemController");
+const userController = require("../controller/userController");
 
 //Item routes
 router.get("/api/listing", itemController.getItem);
 router.post("/api/listing/add", itemController.addItem);
+
+//User routes
+router.post("/api/user/register", userController.registerUser);
+router.post("/api/user/login", userController.loginUser);
 
 module.exports = router;
