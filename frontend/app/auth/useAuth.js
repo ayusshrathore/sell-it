5757061dc14ecf3bcc94f3dff8e438ad914dd796
ayush-rathore <aayushh79@gmail.com>
2021-11-05 12:16:58 +0530

    Update context path

--- frontend/app/auth/useAuth.js
+++ frontend/app/auth/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../auth/context";
 
 const useAuth = () => {
 	const { user, setUser } = useContext(AuthContext);
