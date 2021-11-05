7c8239be8648f979018ff25b10c7e92882aa8ed0
ayush-rathore <aayushh79@gmail.com>
2021-11-05 10:33:42 +0530

    Add conditional render

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import AuthContext from "./app/auth/context";
 import OfflineStatus from "./app/components/OfflineStatus";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import { AppLoading } from "expo";
 
 function App() {
+	const [user, setUser] = useState();
+	const [ready, setReady] = useState(false);
+
+	if (!ready) return <AppLoading onFinish={() => setReady(true)} />;
+
 	return (
-		<>
+		<AuthContext.Provider value={{ user, setUser }}>
 			<OfflineStatus />
 			<NavigationContainer theme={navigationTheme}>
-				<AuthNavigator />
-				{/* <AppNavigator /> */}
+				{user ? <AppNavigator /> : <AuthNavigator />}
 			</NavigationContainer>
-		</>
+		</AuthContext.Provider>
 	);
 }
 
