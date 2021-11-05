702bad6876e2454a01953550fdff46b4743f1bec
ayush-rathore <aayushh79@gmail.com>
2021-11-05 11:41:50 +0530

    Change route

--- frontend/app/api/auth.js
+++ frontend/app/api/auth.js
@@ -1,6 +1,6 @@
 import apiClient from "./client";
 
 const login = (email, password) =>
-	apiClient.post("/api/user/login", { email, password });
+	apiClient.post("/user/login", { email, password });
 
 export default { login };
