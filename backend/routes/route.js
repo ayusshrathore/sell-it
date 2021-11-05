9c843c42136262a02fb54a03113d8c95e5c5df32
ayush-rathore <aayushh79@gmail.com>
2021-11-05 11:23:59 +0530

    Add "/" route

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -13,4 +13,8 @@ router.post("/api/user/register", userController.registerUser);
 router.post("/api/user/login", userController.loginUser);
 router.get("/api/users", userController.getUsers);
 
+router.get("/", (req, res) => {
+	res.status(200).send("Hello World");
+});
+
 module.exports = router;
